fc80c7b68ea7bab4c3bd9b90b801f45e3eeff4e4
vmarc23 <vgmarc2@gmail.com>
2023-10-16 16:50:50 +0200

    Segundo Cambio

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   return runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(
-        title: Text("Marc Vega Gironell - S2AM"),
-      ),
-    ),
-  ));
+      appBar: AppBar(title: const Text("Marc Vega Gironell - S2AM")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(decoration: const BoxDecoration(color: Colors.amber,), 
+            child: const Text("One"),
+          ),
+          Container(decoration: const BoxDecoration(color: Colors.yellowAccent,), 
+            child: const Text("Two"),
+          ),
+          Container(decoration: const BoxDecoration(color: Colors.greenAccent,),         
+            child: const Text("Three"),
+          ),
+        ],
+  ))));
 }
