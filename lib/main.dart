9d42506702e20fb389a1bb0f618e2294bd28831b
vmarc23 <vgmarc2@gmail.com>
2023-10-18 17:38:55 +0200

    Añado Alert

--- lib/main.dart
+++ lib/main.dart
@@ -30,51 +30,42 @@ class _MyCustomFormState extends State<MyCustomForm> {
     super.dispose();
   }
 
-  @override
+ @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Recuperar el valor de un campo de texto'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: TextField(
-          controller: myController,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Recuperar el valor de un campo de texto'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-      showModalBottomSheet(
-              backgroundColor: Colors.transparent,
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: TextField(
+            controller: myController,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showDialog(
               context: context,
               builder: (BuildContext context) {
-                return Container(
-                    decoration: const BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(25),
-                        topRight: Radius.circular(25),
-                      ),
+                return AlertDialog(
+                  title: const Text('Alert dialog'),
+                  content: Text(myController.text), // Muestra el texto introducido
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('OK'),
                     ),
-                    height: 200,
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(myController.text),
-                          ElevatedButton(
-                            onPressed: () => Navigator.of(context).pop(),
-                            child: const Text('Tancar BottomSheet'),
-                          )
-                        ],
-                      ),
-                    ));
-              });
-        },
-        tooltip: 'Mostra el valor!',
-        child: const Icon(Icons.text_fields),
+                  ],
+                );
+              },
+            );
+          },
+          child: const Text('Alerta'),
+        ),
       ),
-    );   
-  }
+    );
+ }
 }
